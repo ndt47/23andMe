9d7881c17b4a7f74cc4c6fdd69046792d0db701a
Nathan Taylor <nathantaylor@me.com>
2019-06-16 12:04:49 -0700

    Initial Commit

---
+++ PhotosViewer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotosViewer
+//
+//  Created by Nathan Taylor on 6/16/19.
+//  Copyright © 2019 Nathan Taylor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
